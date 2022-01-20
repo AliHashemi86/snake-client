1171cfa85d787521b044ae7de747ab5f4e108a71
Ali Hashemi <alihashemmi.ca@gmail.com>
2022-01-19 20:02:43 -0500

    Final

--- client.js
+++ client.js
@@ -1,15 +1,16 @@
 const net = require('net');
+const {IP, PORT} = require('./constants');
 
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
 
   conn.setEncoding("utf8");
 
   conn.on('connect', () => {
-    console.log('Connected to the server!');
+    console.log('Successfully connected to game server!');
     conn.write('Name: ALI');
   //   setTimeout(() => {
   //   conn.write('Move: up')
@@ -23,9 +24,7 @@ const connect = function () {
   return conn;
 };
 
-console.log("Connecting ...");
-
 
 module.exports = {connect};
-//connect();
+
 
