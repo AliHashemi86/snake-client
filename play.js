7b6c49d88eefa18e487945c47e921837cf1cfd34
Ali Hashemi <alihashemmi.ca@gmail.com>
2022-01-19 19:53:24 -0500

    Final

--- play.js
+++ play.js
@@ -1,21 +1,6 @@
-const {connect} = require("./client");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
+console.log("Connecting ...");
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = (data) => {
-  if (data === "\u0003") {
-    process.exit();
-  }
-  //console.log(data);
-};
-
-connect();
-setupInput();
+setupInput(connect());
